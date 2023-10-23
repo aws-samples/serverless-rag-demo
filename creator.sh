0ec021f1f6544b4636f74ae0e12b4b594403e62f
Fraser Sequeira <leo27fraser@gmail.com>
2023-10-23 21:41:16 +0530

    Update creator.sh

--- creator.sh
+++ creator.sh
@@ -34,6 +34,7 @@ echo ' '
 # fi
 
 printf "$Green Please enter your LLM choice (1/2/3/4/5/6/7): $NC"
+printf "\n"
 options=("Amazon Bedrock" "Llama2-7B" "Llama2-13B" "Llama2-70B" "Falcon-7B" "Falcon-40B" "Falcon-180B" "Quit")
 model_id='meta-textgeneration-llama-2-7b-f'
 instance_type='ml.g5.2xlarge'
