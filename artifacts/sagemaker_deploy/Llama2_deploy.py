d10a5fea086fe211191f4a06dd4cfa4ffd29c1c5
Fraser Sequeira <leo27fraser@gmail.com>
2023-12-06 10:46:19 +0530

    Update Llama2_deploy.py

--- artifacts/sagemaker_deploy/Llama2_deploy.py
+++ artifacts/sagemaker_deploy/Llama2_deploy.py
@@ -5,6 +5,6 @@ sagemaker_endpoint_name = str(sys.argv[1])
 model_id, model_version = str(sys.argv[2]), "*"
 
 print(f'Deploying {model_id} with endpoint name {sagemaker_endpoint_name}')
-model = JumpStartModel(model_id=model_id, model_version="3.*")
+model = JumpStartModel(model_id=model_id)
 predictor = model.deploy(endpoint_name=sagemaker_endpoint_name, accept_eula=True)
 
