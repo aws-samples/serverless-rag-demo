5466355a2dcc7c0fe2593728383a0cd70d1d2740
Anup Dutta <duttanup@amazon.com>
2024-07-06 15:25:41 +0530

    Updated home page

--- artifacts/chat-ui/src/pages/home-page.tsx
+++ artifacts/chat-ui/src/pages/home-page.tsx
@@ -1,7 +1,7 @@
 import {
   Container,
   ContentLayout,
-  Box, Grid, ColumnLayout
+  Box, Grid, ColumnLayout, SpaceBetween, Link
 } from "@cloudscape-design/components";
 
 export default function HomePage() {
@@ -48,27 +48,157 @@ export default function HomePage() {
         </Box>
       }
     >
-      <br />
-      <Container>
-        <ColumnLayout borders="vertical" columns={4}>
-          <div>
-            <Box variant="h3">Document Chat</Box>
-            <Box variant="p">A Retrieval Augmented Generation Solution</Box>
-          </div>
-          <div>
-            <Box variant="h3">Multi Agent</Box>
-            <Box variant="p">A solution comprises of multiple-generative AI agents working in tandem to solve a user-problem</Box>
-          </div>
-          <div>
-            <Box variant="h3">Sentiment Analysis</Box>
-            <Box variant="p"></Box>
-          </div>
-          <div>
-            <Box variant="h3">OCR</Box>
-            <Box variant="p"></Box>
-          </div>
-        </ColumnLayout>
-      </Container>
+      <SpaceBetween size="xxl">
+        <Box>&nbsp;</Box>
+        <Container>
+          <ColumnLayout borders="vertical" columns={4}>
+            <div>
+              <Box variant="h3">Document Chat</Box>
+              <Box variant="p">A Retrieval Augmented Generation Solution</Box>
+            </div>
+            <div>
+              <Box variant="h3">Multi Agent</Box>
+              <Box variant="p">A solution comprises of multiple-generative AI agents working in tandem to solve a user-problem</Box>
+            </div>
+            <div>
+              <Box variant="h3">Sentiment Analysis</Box>
+              <Box variant="p"></Box>
+            </div>
+            <div>
+              <Box variant="h3">OCR</Box>
+              <Box variant="p"></Box>
+            </div>
+          </ColumnLayout>
+        </Container>
+
+        <Container
+      media={{
+        content: (
+          <img
+            src="/images/document-chat.png"
+            alt="placeholder"
+          />
+        ),
+        position: "side",
+        width: "14%"
+      }}
+    >
+      <SpaceBetween direction="vertical" size="s">
+        <SpaceBetween direction="vertical" size="xxs">
+          <Box variant="h2">
+            <Link fontSize="heading-m" href="#/document-chat">
+              Document Chat
+            </Link>
+          </Box>
+        </SpaceBetween>
+        <Box variant="p">
+          This is a paragraph. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Ut luctus tempor
+          dolor ac accumsan. This is a paragraph. Lorem
+          ipsum dolor sit amet, consectetur adipiscing
+          elit. Ut luctus tempor dolor ac accumsan.
+        </Box>
+      </SpaceBetween>
+    </Container>
+
+
+    <Container
+      media={{
+        content: (
+          <img
+            src="/images/multi-agent.png"
+            alt="placeholder"
+          />
+        ),
+        position: "side",
+        width: "14%"
+      }}
+    >
+      <SpaceBetween direction="vertical" size="s">
+        <SpaceBetween direction="vertical" size="xxs">
+          <Box variant="h2">
+            <Link fontSize="heading-m" href="#/multi-agent">
+              Multi-Agent
+            </Link>
+          </Box>
+        </SpaceBetween>
+        <Box variant="p">
+          This is a paragraph. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Ut luctus tempor
+          dolor ac accumsan. This is a paragraph. Lorem
+          ipsum dolor sit amet, consectetur adipiscing
+          elit. Ut luctus tempor dolor ac accumsan.
+        </Box>
+      </SpaceBetween>
+    </Container>
+
+
+
+    <Container
+      media={{
+        content: (
+          <img
+            src="/images/sentiment.png"
+            alt="placeholder"
+          />
+        ),
+        position: "side",
+        width: "14%"
+      }}
+    >
+      <SpaceBetween direction="vertical" size="s">
+        <SpaceBetween direction="vertical" size="xxs">
+          <Box variant="h2">
+            <Link fontSize="heading-m" href="#/sentiment-analysis">
+              Sentiment Analysis
+            </Link>
+          </Box>
+        </SpaceBetween>
+        <Box variant="p">
+          This is a paragraph. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Ut luctus tempor
+          dolor ac accumsan. This is a paragraph. Lorem
+          ipsum dolor sit amet, consectetur adipiscing
+          elit. Ut luctus tempor dolor ac accumsan.
+        </Box>
+      </SpaceBetween>
+    </Container>
+
+
+
+    <Container
+      media={{
+        content: (
+          <img
+            src="/images/ocr.png"
+            alt="placeholder"
+          />
+        ),
+        position: "side",
+        width: "14%"
+      }}
+    >
+      <SpaceBetween direction="vertical" size="s">
+        <SpaceBetween direction="vertical" size="xxs">
+          <Box variant="h2">
+            <Link fontSize="heading-m" href="#/ocr">
+              OCR
+            </Link>
+          </Box>
+        </SpaceBetween>
+        <Box variant="p">
+          This is a paragraph. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Ut luctus tempor
+          dolor ac accumsan. This is a paragraph. Lorem
+          ipsum dolor sit amet, consectetur adipiscing
+          elit. Ut luctus tempor dolor ac accumsan.
+        </Box>
+      </SpaceBetween>
+    </Container>
+
+
+      </SpaceBetween>
+
     </ContentLayout>
   );
 }
