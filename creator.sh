4664b206e83cf4ee0acfb239f1edebb97e71a68e
fraseque <fraseque@amazon.com>
2025-04-07 11:05:35 +1000

    Simplify deployment process

--- creator.sh
+++ creator.sh
@@ -17,69 +17,11 @@ then
 fi
 echo "Environment: $infra_env"
 
-deployment_region=$(curl -s http://169.254.169.254/task/AvailabilityZone | sed 's/\(.*\)[a-z]/\1/')
+deployment_region=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].RegionName')
 embed_model_id='cohere.embed-english-v3'
-if [ -z "$deployment_region" ]
-then
-    printf  "$Red !!! Cannot detect region. Manually select your AWS Cloudshell region from the below list $NC"
-    printf "\n"
-    printf "$Green Please enter your current AWS cloudshell region (1/2/3/4/5/6): $NC"
-    printf "\n"
-    region_options=("N-Virginia" "Oregon" "Tokyo" "Mumbai" "Frankfurt" "Sydney" "Quit")
-    select region_opts in "${region_options[@]}"
-    do
-        case $region_opts in 
-            "N-Virginia")
-                deployment_region='us-east-1'
-                printf "$Green Deploy in US East(N.Virginia) $NC"
-                printf "$Green Embedding model $embed_model_id $NC"
-                printf "\n"
-                ;; 
-            "Oregon")
-                deployment_region='us-west-2'
-                printf "$Green Deploy in US West(Oregon) $NC"
-                printf "$Green Embedding model $embed_model_id $NC"
-                ;;
-            # "ap-southeast-1")
-            #     deployment_region='ap-southeast-1'
-            #     printf "$Green Deploy in Asia Pacific (Singapore) $NC"
-            #     ;;
-            "Tokyo")
-                deployment_region='ap-northeast-1'
-                printf "$Green Deploy in Asia Pacific (Tokyo) $NC"
-                embed_model_id='cohere.embed-english-v3'
-                printf "$Green Embedding model $embed_model_id $NC"
-                ;;
-            "Mumbai")
-                deployment_region='ap-south-1'
-                printf "$Green Deploy in Asia Pacific (Mumbai) $NC"
-                embed_model_id='cohere.embed-english-v3'
-                printf "$Green Embedding model $embed_model_id $NC"
-                ;;
-            "Frankfurt")
-                deployment_region='eu-central-1'
-                printf "$Green Deploy in Europe (Frankfurt) $NC"
-                printf "$Green Embedding model $embed_model_id $NC"
-                ;;
-            "Sydney")
-                deployment_region='ap-southeast-2'
-                printf "$Green Deploy in Asia Pacific (Sydney) $NC"
-                embed_model_id='cohere.embed-english-v3'
-                printf "$Green Embedding model $embed_model_id $NC"
-                ;;
-            "Quit")
-                printf "$Red Quit deployment $NC"
-                exit 1
-                break
-                ;;
-            *)
-            printf "$Red Exiting, Invalid option $REPLY. Select from 1/2/3/4/5/6 $NC"
-            exit 1;;
-        esac
-        break
-    done
-fi
-echo "Selected region: $deployment_region "
+
+printf "$Green Selected region: $deployment_region $NC"
+printf "$Green Selected embedding model: $embed_model_id $NC"
 
 echo '*************************************************************'
 echo ' '
